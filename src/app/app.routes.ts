bfde1b94e53d1726fd58ee7672b4f3cfbdeda98a
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-02-23 18:36:36 +0100

    preventivi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {Preventivi} from './preventivi/preventivi';
 import {TaglioPannelli} from './taglio-pannelli/taglio-pannelli';
 import {Prezzario} from './prezzario/prezzario';
 import {Home} from './home/home';
+import {ListaPreventivi} from './lista-preventivi/lista-preventivi';
 
 export const routes: Routes = [
   {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "home", component: Home},
+  {path: "lista-preventivi", component: ListaPreventivi},
   {path: "preventivi", component: Preventivi},
   {path: "taglio-pannelli", component: TaglioPannelli},
   {path: "prezzario", component: Prezzario}
