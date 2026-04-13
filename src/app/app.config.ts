026ccd95838a3e61ea9378bb7021985215b8c76d
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-04-13 11:46:16 +0200

    Aggiunti commenti esplicativi per migliorare la leggibilità della configurazione dell'app e l'inizializzazione del router.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,15 @@ import {provideRouter} from '@angular/router';
 
 // Importa l'elenco delle rotte definite in app.routes.ts
 import {routes} from './app.routes';
-// Importiamo withInterceptors
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './auth/auth.interceptor'; // Importiamo il nostro interceptor
 
 export const appConfig: ApplicationConfig = {
+
+  // Providers globali iniettabili tramite Dependency Injection (DI)
   providers: [
     provideBrowserGlobalErrorListeners(),
+    // inizializza il router con le rotte definite in app.routes.ts
     provideRouter(routes),
     // Aggiungiamo l'interceptor al client HTTP
     provideHttpClient(withInterceptors([authInterceptor]))
